cf72ed7618e4a34fa1f940580c9ace268575f9c9
DawnGate <lam99quyen@gmail.com>
2022-11-09 10:30:45 +0700

    first init setting for nextjs app

---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../styles/Navbar.module.css";
+
+function Navbar() {
+  return <div className={style.container}>Navbar</div>;
+}
+
+export default Navbar;
